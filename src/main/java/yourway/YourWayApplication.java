9b447cb8ae50007d224405d829fa5ce2f9f955d1
Danil <matrosovdan@mail.ru>
2024-01-06 22:47:32 +0300

    initial commit

---
+++ src/main/java/yourway/YourWayApplication.java
@@ -0,0 +1,13 @@
+package yourway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourWayApplication.class, args);
+	}
+
+}
