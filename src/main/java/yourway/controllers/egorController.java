f33169bee06060088d051940508446f20508b171
Egor <egor.belosludtsev@mail.ru>
2024-01-15 00:55:30 +0300

    egor's first commit (add egorController)

---
+++ src/main/java/yourway/controllers/egorController.java
@@ -0,0 +1,12 @@
+package yourway.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class egorController {
+    @GetMapping("/hello")
+    public void hello(){
+        System.out.println("Hello world from egor's branch");
+    }
+}
